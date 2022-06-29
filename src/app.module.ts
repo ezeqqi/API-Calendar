6910e5bb750a6a14acfe8fee6a3839f6a9e69648
Ezeqqi <ezequiel.l.silveira@gmail.com>
2022-06-29 02:34:24 -0300

    feat: Events module and mongoose dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventsModule } from './events/events.module';
+
 
 @Module({
-  imports: [],
+  imports: [EventsModule, MongooseModule.forRoot('mongodb+srv://ezeqqi:mongopass@cluster01.w7zhv.mongodb.net/?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
