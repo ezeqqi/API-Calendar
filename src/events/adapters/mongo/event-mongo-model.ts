7562a3710a536997283e29a4519354724fe424d5
Ezeqqi <ezequiel.l.silveira@gmail.com>
2022-07-25 13:44:29 -0300

    feat: install mongoose module into project

---
+++ src/events/adapters/mongo/event-mongo-model.ts
@@ -0,0 +1,26 @@
+import { MongooseModule, Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type EventDocument = Event & Document;
+
+@Schema()
+export class Event {
+    @Prop({required: true})
+    date: string;
+
+    @Prop({required: true})
+    name: string;
+
+    @Prop({required: true})
+    description: string;
+}
+
+export const EventSchema = SchemaFactory.createForClass(Event);
+
+export const EventMongoProvider = MongooseModule.forFeature([
+    {
+        name: 'EventsMongo',
+        collection: 'Events',
+        schema: EventSchema,
+    }
+])
