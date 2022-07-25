3c89bd18c7a772ad079f995f729ee6017b33d63a
Ezeqqi <ezequiel.l.silveira@gmail.com>
2022-07-25 13:45:55 -0300

    feat: events service and controller

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,26 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { EventDocument, Event } from './adapters/mongo/event-mongo-model';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
 
+
 @Injectable()
 export class EventsService {
+  constructor(@InjectModel(Event.name) private eventModel: Model<EventDocument>) {}
+
   create(createEventDto: CreateEventDto) {
-    return 'This action adds a new event';
+    const event = new this.eventModel(createEventDto)
+    return event.save();
   }
 
   findAll() {
-    return `This action returns all events`;
+    return this.eventModel.find()
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} event`;
+  findOne(id: string) {
+    return this.eventModel.findById(id)
   }
 
-  update(id: number, updateEventDto: UpdateEventDto) {
-    return `This action updates a #${id} event`;
+  update(id: string, updateEventDto: UpdateEventDto) {
+    return this.eventModel.findByIdAndUpdate(
+      {
+      _id: id
+      },
+      {
+        $set: updateEventDto,
+      },
+      {
+        new: true,
+      }
+    )
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} event`;
+  remove(id: string) {
+    return this.eventModel.deleteOne({
+      _id: id,
+    }).exec()
   }
 }
